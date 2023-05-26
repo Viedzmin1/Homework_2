826b0b0720588c5fb46c5b332e6b46a54058e100
Aleksey Rodionov <rodion_obn@mail.ru>
2023-05-26 19:30:10 +0300

    Решена вторая задача

--- Program.cs
+++ Program.cs
@@ -4,11 +4,34 @@
 // 782 -> 8
 // 918 -> 1
 
-System.Console.WriteLine("Введите трехзначное число ");
+// System.Console.WriteLine("Введите трехзначное число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int x1 = num/10;
+// int x2 = x1%10;
+// System.Console.WriteLine($"число {x2}");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Через строку решать нельзя.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
-int x1 = num/10;
-int x2 = x1%10;
-System.Console.WriteLine($"число {x2}");
-
-
-
+    if (num>100)
+    {
+        double razr = Math.Log10(num);
+        int i = 1;
+        while (i<razr-2)
+        {
+            num = num/10;
+            i++;
+        }
+        num = num%10;
+        System.Console.WriteLine($"третье число равно  {num}");
+    }
+    else
+    {
+        System.Console.WriteLine($"в числе {num} нет третьей цифры");
+    }
